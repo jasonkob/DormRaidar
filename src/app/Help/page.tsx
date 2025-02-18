ea486ca04c542047bd8e27e0202bdb4e6d83aa1d
Nonthawat Phongsrinual <icementel.11@gmail.com>
2025-02-18 11:22:06 +0700

    change error not use image

--- src/app/Help/page.tsx
+++ src/app/Help/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from "next/link";
-import Image from 'next/image';
 import { Search, Menu, User } from 'lucide-react';
 
 export default function ContactPage() {
