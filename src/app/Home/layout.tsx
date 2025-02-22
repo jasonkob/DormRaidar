0e817c2e9eb9583071beeeceb22b1c74f526edf5
pithawat <linkpittawat@gmail.com>
2025-02-22 23:49:59 +0700

    Home page
    adding homepage and user layout

---
+++ src/app/Home/layout.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { useState } from "react"
+import Link from "next/link"
+
+export default function userLayout({
+    children}:{
+        children:React.ReactNode
+    }){
+    
+    const [shwMenu,setShwMenu] = useState(false)
+    const showMenu = ()=>{
+        setShwMenu(!shwMenu)
+    }
+    return(
+        <div className="px-4 py-2 flex flex-col justify-center">
+            <div className="flex justify-between items-center h-[80px] z-10">
+                <div className="flex flex-row ">
+                    <Link href="/Home">
+                        <img className="w-28 h-20" src='/logo.png' alt="logo" />
+                    </Link>
+                    <div className="flex items-center mx-8">
+                        <Link href="/Home" className="mx-4 text-base  transition-all duration-200 ease-out hover:font-semibold hover:underline">Home</Link>
+                        <Link href="/Mybooking" className="mx-4 text-base transition-all duration-200 ease-out hover:font-semibold hover:underline">My booking</Link>
+                        <Link href="/Finddorm" className="mx-4 text-base transition-all duration-200 ease-out hover:font-semibold hover:underline">Find Dorm</Link>
+                    </div>
+                </div>
+                <div className="relative flex flex-col justify-center mr-4  ">
+                    <div className="flex flex-row items-center  p-2 border-2  rounded-xl cursor-pointer transition-all duration-150 ease-in-out  hover:shadow-xl" onClick={showMenu}>
+                        <svg className="mx-2" width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 1.28571C0 0.944722 0.131696 0.617695 0.366117 0.376577C0.600537 0.135459 0.918479 0 1.25 0H18.75C19.0815 0 19.3995 0.135459 19.6339 0.376577C19.8683 0.617695 20 0.944722 20 1.28571C20 1.62671 19.8683 1.95373 19.6339 2.19485C19.3995 2.43597 19.0815 2.57143 18.75 2.57143H1.25C0.918479 2.57143 0.600537 2.43597 0.366117 2.19485C0.131696 1.95373 0 1.62671 0 1.28571ZM0 9C0 8.65901 0.131696 8.33198 0.366117 8.09086C0.600537 7.84974 0.918479 7.71429 1.25 7.71429H18.75C19.0815 7.71429 19.3995 7.84974 19.6339 8.09086C19.8683 8.33198 20 8.65901 20 9C20 9.34099 19.8683 9.66802 19.6339 9.90914C19.3995 10.1503 19.0815 10.2857 18.75 10.2857H1.25C0.918479 10.2857 0.600537 10.1503 0.366117 9.90914C0.131696 9.66802 0 9.34099 0 9ZM1.25 15.4286C0.918479 15.4286 0.600537 15.564 0.366117 15.8051C0.131696 16.0463 0 16.3733 0 16.7143C0 17.0553 0.131696 17.3823 0.366117 17.6234C0.600537 17.8645 0.918479 18 1.25 18H18.75C19.0815 18 19.3995 17.8645 19.6339 17.6234C19.8683 17.3823 20 17.0553 20 16.7143C20 16.3733 19.8683 16.0463 19.6339 15.8051C19.3995 15.564 19.0815 15.4286 18.75 15.4286H1.25Z" fill="black"/>
+                        </svg>
+                        <svg className="mx-2" width="30" height="30" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clipPath="url(#clip0_120_327)">
+                            <path d="M40 19.98C40 8.95 31.04 0 20 0C8.96 0 0 8.95 0 19.98C0 26.055 2.76 31.53 7.08 35.205C7.12 35.245 7.16 35.245 7.16 35.285C7.52 35.565 7.88 35.845 8.28 36.125C8.48 36.245 8.64 36.4025 8.84 36.5625C12.1465 38.8006 16.0472 39.9978 20.04 40C24.0328 39.9978 27.9335 38.8006 31.24 36.5625C31.44 36.4425 31.6 36.285 31.8 36.1625C32.16 35.885 32.56 35.605 32.92 35.325C32.96 35.285 33 35.285 33 35.245C37.24 31.5275 40 26.055 40 19.98ZM20 37.4825C16.24 37.4825 12.8 36.2825 9.96 34.285C10 33.965 10.08 33.6475 10.16 33.3275C10.3991 32.4605 10.7487 31.6279 11.2 30.85C11.64 30.09 12.16 29.41 12.8 28.81C13.4 28.21 14.12 27.6525 14.84 27.2125C15.6 26.7725 16.4 26.4525 17.28 26.2125C18.1671 25.9749 19.0816 25.8547 20 25.855C22.7269 25.8344 25.3538 26.8805 27.32 28.77C28.24 29.69 28.96 30.7692 29.48 32.0075C29.76 32.7275 29.96 33.4867 30.08 34.285C27.1279 36.3604 23.6086 37.4768 20 37.4825ZM13.88 18.9825C13.5283 18.1753 13.3511 17.3029 13.36 16.4225C13.36 15.545 13.52 14.665 13.88 13.865C14.24 13.065 14.72 12.3475 15.32 11.7475C15.92 11.1475 16.64 10.67 17.44 10.31C18.24 9.95 19.12 9.79 20 9.79C20.92 9.79 21.76 9.95 22.56 10.31C23.36 10.67 24.08 11.15 24.68 11.7475C25.28 12.3475 25.76 13.0675 26.12 13.865C26.48 14.665 26.64 15.545 26.64 16.4225C26.64 17.3425 26.48 18.1825 26.12 18.98C25.7746 19.7693 25.2864 20.488 24.68 21.1C24.0678 21.7055 23.3491 22.1928 22.56 22.5375C20.9068 23.2154 19.0532 23.2154 17.4 22.5375C16.6109 22.1928 15.8922 21.7055 15.28 21.1C14.6731 20.4966 14.1966 19.7776 13.88 18.9825ZM32.44 32.2475C32.44 32.1675 32.4 32.1275 32.4 32.0475C32.0073 30.7958 31.4274 29.6107 30.68 28.5325C29.932 27.4464 29.0135 26.4883 27.96 25.695C27.1551 25.0893 26.2826 24.5792 25.36 24.175C25.7778 23.8955 26.1664 23.5747 26.52 23.2175C27.1163 22.6288 27.6401 21.9707 28.08 21.2575C28.9689 19.8033 29.4267 18.1266 29.4 16.4225C29.4131 15.1612 29.1681 13.9106 28.68 12.7475C28.1987 11.6266 27.5061 10.6089 26.64 9.75C25.773 8.90262 24.7556 8.22436 23.64 7.75C22.4752 7.26213 21.2228 7.01795 19.96 7.0325C18.697 7.01874 17.4446 7.26378 16.28 7.7525C15.1521 8.22107 14.1314 8.91431 13.28 9.79C12.4282 10.6524 11.7493 11.6699 11.28 12.7875C10.7919 13.9506 10.5469 15.2012 10.56 16.4625C10.56 17.3425 10.68 18.1817 10.92 18.98C11.16 19.82 11.48 20.58 11.92 21.2975C12.32 22.0175 12.88 22.6575 13.48 23.2575C13.84 23.6175 14.24 23.9367 14.68 24.215C13.7532 24.6276 12.8802 25.1514 12.08 25.775C11.04 26.575 10.12 27.5325 9.36 28.5725C8.60568 29.6466 8.02524 30.8328 7.64 32.0875C7.6 32.1675 7.6 32.2475 7.6 32.2875C4.44 29.09 2.48 24.775 2.48 19.98C2.48 10.35 10.36 2.4775 20 2.4775C29.64 2.4775 37.52 10.35 37.52 19.98C37.5148 24.5799 35.6883 28.9906 32.44 32.2475Z" fill="black"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_120_327">
+                                <rect width="40" height="40" fill="white"/>
+                            </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                    {shwMenu && (
+                        <div className="absolute -bottom-[175px] right-0 w-[200px] h-[165px] border bg-white rounded-lg">
+                            <ul className="border-b-2 mt-1">
+                                <li className="cursor-pointer select-none transition duration-150 ease-in-out  hover:bg-yellow-200 "><Link href="/signup"><p  className="px-4 py-2 text-[14px] font-semibold">Sign up</p></Link></li>
+                                <li className="cursor-pointer select-none transition duration-150 ease-in-out  hover:bg-yellow-200 "><Link href="/Login"><p className="px-4 py-2 text-[14px]">Login</p></Link></li>
+                            </ul>
+                            <ul className=" mt-1 mb-1">
+                                <li className="cursor-pointer select-none transition duration-150 ease-in-out  hover:bg-yellow-200 "><Link href="/signup" ><p className="px-4 py-2 text-[14px]">Become Dorm Admin</p></Link></li>
+                                <li className="cursor-pointer select-none transition duration-150 ease-in-out  hover:bg-yellow-200 "><Link href="/Login" ><p className="px-4 py-2 text-[14px]">Dorm Admin Login</p></Link></li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <div className="flex justify-center px-2">
+                {children}
+            </div>
+        </div>
+    )
+}
---
+++ src/app/Home/page.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import { useState } from "react"
+
+export default  function Home (){
+    const [location,setLocation] = useState('Select Location')
+    const [price,setPrice] = useState(0)
+    const [shwLocation,setShwLocation] = useState(false)
+    const [shwPrice,setShwPrice] = useState(false)
+
+    const allLocation = [
+        {id:1 ,content:'kmitl'},
+        {id:2 ,content:'kmutnb'},
+        {id:3 ,content:'kmutt'}
+    ]
+
+    const allPrice =[
+        {id:1 ,price:4000},
+        {id:2 ,price:5000},
+        {id:3 ,price:6000}
+    ]
+
+    const showLocation = ()=>{
+        setShwPrice(false)
+        setShwLocation(!shwLocation)
+    }
+    const showPrice = ()=>{
+        setShwLocation(false)
+        setShwPrice(!shwPrice)
+    }
+    
+    const selectLocation = ({location}:{location:string})=>{
+        setLocation(location)
+        setShwLocation(false)
+    }
+    const selectPrice = ({price}:{price:number})=>{
+        setPrice(price)
+        setShwPrice(false)
+    }
+
+    const handleExplore = ()=>{
+        console.log('explore')
+    }
+
+    return(
+        <div className="bg-[url('/home.jpg')]  bg-cover bg-center h-screen w-full rounded-3xl opacity-90">
+            <div className="flex h-1/4 w-full justify-center items-end">
+                <div className='flex items-center bg-white px-4 pt-4  h-[110px] w-[900px] rounded-xl opacity-90'>
+                    <section className='flex flex-col h-full w-4/12 '>
+                        <label htmlFor="Location" className='text-[24px] font-bold mb-2 select-none '>Location</label>
+                        <div className='w-10/12 flex items-center border border-black rounded-md mb-3 cursor-pointer' onClick={showLocation}>
+                            <span className='mx-2 mr-4'>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12 11.5C11.337 11.5 10.7011 11.2366 10.2322 10.7678C9.76339 10.2989 9.5 9.66304 9.5 9C9.5 8.33696 9.76339 7.70107 10.2322 7.23223C10.7011 6.76339 11.337 6.5 12 6.5C12.663 6.5 13.2989 6.76339 13.7678 7.23223C14.2366 7.70107 14.5 8.33696 14.5 9C14.5 9.3283 14.4353 9.65339 14.3097 9.95671C14.1841 10.26 13.9999 10.5356 13.7678 10.7678C13.5356 10.9999 13.26 11.1841 12.9567 11.3097C12.6534 11.4353 12.3283 11.5 12 11.5ZM12 2C10.1435 2 8.36301 2.7375 7.05025 4.05025C5.7375 5.36301 5 7.14348 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9C19 7.14348 18.2625 5.36301 16.9497 4.05025C15.637 2.7375 13.8565 2 12 2Z" fill="black"/>
+                                </svg>
+                            </span>
+                            <span className="w-3/5 select-none">{location}</span>
+                            <span className='ml-1' >
+                                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.667 16.6665L20.0003 24.9998L28.3337 16.6665H11.667Z" fill="black"/>
+                                </svg>
+                            </span>
+                        </div>
+                        {shwLocation && (
+                            <div className='bg-white border rounded-md w-10/12 '>
+                                <ul className=''>
+                                    {allLocation.map((item,index)=>(
+                                        <li className='p-2 rounded-md pl-4 cursor-pointer select-none transition duration-100 ease-in hover:bg-yellow-200' key={index} onClick={()=>selectLocation({location:item.content})}>{item.content}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
+                    </section>
+                    <section className='flex flex-col h-full w-4/12'>
+                        <label htmlFor="Location" className='text-[24px] font-bold mb-2 select-none'>Price</label>
+                        <div className='w-10/12 flex items-center border border-black rounded-md mb-3 cursor-pointer' onClick={showPrice}>
+                            <span className='mx-2 mr-4 '>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M16.1369 4.72776L17.9669 6.55776C20.6559 9.24776 21.9999 10.5918 21.9999 12.2618C21.9999 13.9328 20.6559 15.2768 17.9669 17.9658C15.2769 20.6558 13.9329 21.9998 12.2619 21.9998C10.5919 21.9998 9.24687 20.6558 6.55787 17.9668L4.72787 16.1368C3.18287 14.5908 2.40987 13.8188 2.12287 12.8158C1.83487 11.8128 2.08087 10.7478 2.57287 8.61876L2.85587 7.39076C3.26887 5.59876 3.47587 4.70276 4.08887 4.08876C4.70187 3.47476 5.59887 3.26876 7.39087 2.85576L8.61887 2.57176C10.7489 2.08076 11.8129 1.83476 12.8159 2.12176C13.8189 2.40976 14.5919 3.18276 16.1369 4.72776ZM11.1459 14.3278C10.4729 13.6558 10.4779 12.6898 10.8809 11.9248C10.7811 11.7807 10.735 11.6062 10.7504 11.4316C10.7659 11.257 10.8421 11.0933 10.9657 10.969C11.0892 10.8447 11.2524 10.7677 11.4269 10.7512C11.6014 10.7347 11.7762 10.7798 11.9209 10.8788C12.2609 10.6988 12.6339 10.6028 13.0059 10.6068C13.2048 10.6086 13.3948 10.6894 13.5341 10.8314C13.6735 10.9733 13.7507 11.1648 13.7489 11.3638C13.747 11.5627 13.6662 11.7527 13.5242 11.892C13.3823 12.0314 13.1908 12.1086 12.9919 12.1068C12.7603 12.1153 12.5415 12.2148 12.3829 12.3838C11.9959 12.7708 12.0969 13.1588 12.2059 13.2678C12.3159 13.3768 12.7029 13.4778 13.0899 13.0908C13.8739 12.3068 15.2279 12.0468 16.0949 12.9138C16.7679 13.5868 16.7629 14.5528 16.3599 15.3178C16.459 15.4619 16.5046 15.6362 16.4889 15.8104C16.4731 15.9846 16.3969 16.1478 16.2735 16.2718C16.1501 16.3958 15.9873 16.4728 15.8131 16.4894C15.639 16.506 15.4645 16.4612 15.3199 16.3628C14.8711 16.609 14.3506 16.691 13.8479 16.5948C13.6529 16.5547 13.4819 16.4389 13.3724 16.2727C13.2628 16.1066 13.2238 15.9037 13.2639 15.7088C13.3039 15.5138 13.4198 15.3428 13.5859 15.2333C13.7521 15.1237 13.9549 15.0847 14.1499 15.1248C14.3269 15.1618 14.6129 15.1038 14.8579 14.8588C15.2449 14.4708 15.1439 14.0838 15.0349 13.9748C14.9249 13.8658 14.5379 13.7648 14.1509 14.1518C13.3669 14.9358 12.0129 15.1958 11.1459 14.3278ZM10.0199 10.2928C10.2056 10.107 10.3528 9.88649 10.4533 9.64382C10.5538 9.40114 10.6055 9.14105 10.6054 8.8784C10.6054 8.61575 10.5536 8.35568 10.453 8.11304C10.3525 7.8704 10.2051 7.64995 10.0194 7.46426C9.83361 7.27857 9.6131 7.13128 9.37043 7.03082C9.12775 6.93035 8.86766 6.87866 8.60501 6.87871C8.34236 6.87875 8.08229 6.93053 7.83965 7.03109C7.59701 7.13164 7.37656 7.279 7.19087 7.46476C6.81585 7.83991 6.60522 8.34866 6.60531 8.87911C6.60541 9.40956 6.81622 9.91824 7.19137 10.2933C7.56652 10.6683 8.07527 10.8789 8.60572 10.8788C9.13617 10.8787 9.64485 10.6679 10.0199 10.2928Z" fill="black"/>
+                                </svg>
+                            </span>
+                            <span className="w-3/5  select-none">{price? "less than":"Insert Price"}<span className="ml-2 text-black">{price? price:""}</span></span>
+                            <span className='ml-1' >
+                                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.667 16.6665L20.0003 24.9998L28.3337 16.6665H11.667Z" fill="black"/>
+                                </svg>
+                            </span>
+                        </div>
+                        {shwPrice && (
+                            <div className='bg-white border rounded-md w-10/12'>
+                                <ul className=''>
+                                    {allPrice.map((item,index)=>(
+                                        <li className='p-2 rounded-md pl-4 cursor-pointer select-none transition duration-100 ease-in hover:bg-yellow-200' key={index} onClick={()=>selectPrice({price:item.price})}>{item.price}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
+                    </section>
+                    <div className="w-4/12">
+                        <button className="w-11/12 bg-yellow-300 p-2 rounded-lg text-[22px] font-bold shadow-md select-none transition duration-200 ease-in hover:bg-yellow-400 " onClick={handleExplore}>Explore</button>
+                    </div>
+                </div>
+            </div>
+            <div className='h-4/6 w-8/12 flex flex-col place-self-end items-center justify-center text-center overflow-hidden'>
+                <p className='text-[28px] text-white text-center mt-20'>Discover your perfect dorm —</p>
+                <p className='text-[18px] text-white self-center'>your smart choice for a comfortable and connected <br/> student life!</p>
+            </div>
+        </div>  
+    )
+}
