09f8cb4aa304aa71a2cb25ce363ac5e3bac3f3c1
Downueaa_ <162528777+Downuea2545@users.noreply.github.com>
2025-02-23 11:34:39 +0700

    กำหนดให้เปิดเว็บขึ้น /Home

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from 'next'
- 
+
 const nextConfig: NextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: false,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/Home',
+        permanent: true, // ถ้าต้องการให้เป็น redirect แบบถาวร
+      },
+    ]
+  },
 }
- 
+
 export default nextConfig
